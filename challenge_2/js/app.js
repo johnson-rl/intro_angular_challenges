3c4a03032f4bd5db8709812906100ea45c028fa4
johnson-rl <johnson.rl@gmail.com>
2017-01-04 16:26:45 -0800

    all challenges complete

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,25 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
-//
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME";
-// }
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("WDIController", WDIController);
+
+function WelcomeController(){
+  this.full_name = "Ryan Johnson";
+  this.age = 33;
+  this.city = 'Sacramento';
+  this.state = 'California';
+  this.shout = function(str){
+    return str.toUpperCase() + '!';
+  }
+}
+
+function WDIController () {
+  this.class_name = "WDI 34";
+  this.enrolled_students = "Shiv, Amber, Mike, Ryan, RJ, AA-Ron, LD, Ricardo, Greice, and Alex";
+  this.start_date = 'November 28, 2016';
+  this.end_date = 'March 3, 2017';
+  this.daysRemaining = function(date){
+    return Math.ceil((Date.parse(date) - Date.now())/86400000);
+  }
+}
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,23 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController)
+app.controller("loanController", loanController)
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+
+function loanController (){
+  this.amount = 1000;
+  this.apr = 3.5;
+  this.month = function(m){
+    var amt = this.amount * Math.pow(Math.pow(Math.E, (m/12)), (this.apr/100))
+    console.log(amt)
+    return {
+      accrued_interest : Math.round((amt - this.amount)*100)/100,
+      total_balance : Math.round(amt*100)/100
+    }
+  }
+}
